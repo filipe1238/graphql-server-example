3966eee04639f3f574b600e5d080efd62d9d3851
Filipe <83041450+filipe1238@users.noreply.github.com>
2024-03-25 22:15:24 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
 var cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    // http://localhost:3000
+    // `https://multiplayer-chess-site.onrender.com`
+    cors: {
+        origins: `https://graphql-server-example-5pid.onrender.com/`,
+
+        // location of frontend (need to somehow specify port to render so that this code works)
+        // I might be able just pass the render site link
+        // I also forgot to add socket.io connect link to frontend in chessGame which is probably why I received an error
+        methods: ["GET", "POST"],
+    },
+    pingInterval: 2000,
+    pingTimeout: 10000,
+});
 
 
-app.use(cors({
-    origin: 'https://graphql-server-example-5pid.onrender.com/',
-}));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/static/index.html');
@@ -32,6 +46,6 @@ io.on("connection", (socket) => {
 
 
 
-server.listen(10000, () => {
-    console.log('listening on *:10000');
+server.listen(port, () => {
+    console.log('listening on *:http://localhost:' + port);
 });
