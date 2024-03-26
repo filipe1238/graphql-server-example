fa36654cbc4bd2d9f8c04340bdaf6741fc2dfb9d
Filipe <83041450+filipe1238@users.noreply.github.com>
2024-03-25 21:03:34 -0300

    update

--- src/index.js
+++ src/index.js
@@ -6,8 +6,23 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const cors = require('cors');
 
+var allowedOrigins = ['http://localhost:3000',
+                                'http://https://graphql-server-example-5pid.onrender.com/'];
 
-app.use(cors());
+
+app.use(cors({
+    origin: function(origin, callback){
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        var msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  }));
 
 // app.get('/', (req, res) => {
 //     res.sendFile(__dirname + '/index.html');
