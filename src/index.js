73fb80ff3621fdff6e843201ff225f48211b0c29
Filipe <83041450+filipe1238@users.noreply.github.com>
2024-03-25 22:18:14 -0300

    update

--- src/index.js
+++ src/index.js
@@ -9,14 +9,8 @@ app.use(cors());
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    // http://localhost:3000
-    // `https://multiplayer-chess-site.onrender.com`
     cors: {
         origins: `https://graphql-server-example-5pid.onrender.com/`,
-
-        // location of frontend (need to somehow specify port to render so that this code works)
-        // I might be able just pass the render site link
-        // I also forgot to add socket.io connect link to frontend in chessGame which is probably why I received an error
         methods: ["GET", "POST"],
     },
     pingInterval: 2000,
