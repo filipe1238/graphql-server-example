4873fb7488fbe0c4f40046b256608e113b4ab677
Filipe <83041450+filipe1238@users.noreply.github.com>
2024-03-25 21:06:20 -0300

    update

--- src/index.js
+++ src/index.js
@@ -4,25 +4,14 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+
 const cors = require('cors');
-
-var allowedOrigins = ['http://localhost:3000',
-                                'http://https://graphql-server-example-5pid.onrender.com/'];
-
-
-app.use(cors({
-    origin: function(origin, callback){
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if(!origin) return callback(null, true);
-      if(allowedOrigins.indexOf(origin) === -1){
-        var msg = 'The CORS policy for this site does not ' +
-                  'allow access from the specified Origin.';
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    }
-  }));
+const corsOptions ={
+    origin:'http://localhost:3000',
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 // app.get('/', (req, res) => {
 //     res.sendFile(__dirname + '/index.html');
